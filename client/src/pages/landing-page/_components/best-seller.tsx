c2c4733cbe540097a027bd9974d9af37e475b779
Lolillya <someonesusernam@gmail.com>
2025-08-08 01:04:42 +0800

    added best seller section on landing page

--- client/src/pages/landing-page/_components/best-seller.tsx
+++ client/src/pages/landing-page/_components/best-seller.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button';
+import coffee1 from '@/assets/mid1.png';
 
 export const BestSellerSection = () => {
   return (
-    <section className="">
+    <section className="py-20">
       <div className="container">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center py-20">
           <h2>best selling coffee</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus optio
@@ -12,51 +13,91 @@ export const BestSellerSection = () => {
           </p>
         </div>
 
-        <div className="flex gap-10">
+        <div className="flex gap-10 my-10">
           {/* PRODUCT CARD */}
 
-          <div>
-            <span>#1 selling</span>
-            <h3>Double Espresso</h3>
+          <div className="rounded-lg shadow-md py-10 px-8 hover:shadow-lg transition-all duration-200">
+            <div className="flex relative">
+              <div className="flex flex-col justify-center w-full">
+                <span>#1 selling</span>
+                <h3>Double Espresso</h3>
+              </div>
+              <div className="w-full"></div>
+              <img
+                src={coffee1}
+                alt="coffee product"
+                width={230}
+                className="absolute -right-[1rem] -top-[8rem]"
+              />
+            </div>
+            <div className="mt-10 flex flex-col gap-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                dicta alias id, nostrum rerum cum ducimus omnis sequi cumque
+                nam.
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-              dicta alias id, nostrum rerum cum ducimus omnis sequi cumque nam.
-            </p>
-
-            <div className="flex w-full justify-between items-center">
-              <label>$ 50.99</label>
-              <Button variant="1">order now</Button>
+              <div className="flex w-full justify-between items-center">
+                <strong>$ 50.99</strong>
+                <Button variant="1">order now</Button>
+              </div>
             </div>
           </div>
 
-          <div>
-            <span>#1 selling</span>
-            <h3>Double Espresso</h3>
+          <div className="rounded-lg shadow-md py-10 px-8 hover:shadow-lg transition-all duration-200">
+            <div className="flex relative">
+              <div className="flex flex-col justify-center w-full">
+                <span>#1 selling</span>
+                <h3>Double Espresso</h3>
+              </div>
+              <div className="w-full"></div>
+              <img
+                src={coffee1}
+                alt="coffee product"
+                width={230}
+                className="absolute -right-[1rem] -top-[8rem]"
+              />
+            </div>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-              dicta alias id, nostrum rerum cum ducimus omnis sequi cumque nam.
-            </p>
+            <div className="mt-10 flex flex-col gap-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                dicta alias id, nostrum rerum cum ducimus omnis sequi cumque
+                nam.
+              </p>
 
-            <div className="flex w-full justify-between items-center">
-              <label>$ 50.99</label>
-              <Button variant="1">order now</Button>
+              <div className="flex w-full justify-between items-center">
+                <strong>$ 50.99</strong>
+                <Button variant="1">order now</Button>
+              </div>
             </div>
           </div>
 
-          <div>
-            <span>#1 selling</span>
-            <h3>Double Espresso</h3>
+          <div className="rounded-lg shadow-md py-10 px-8 hover:shadow-lg transition-all duration-200">
+            <div className="flex relative">
+              <div className="flex flex-col justify-center w-full">
+                <span>#1 selling</span>
+                <h3>Double Espresso</h3>
+              </div>
+              <div className="w-full"></div>
+              <img
+                src={coffee1}
+                alt="coffee product"
+                width={230}
+                className="absolute -right-[1rem] -top-[8rem]"
+              />
+            </div>
+            <div className="mt-10 flex flex-col gap-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                dicta alias id, nostrum rerum cum ducimus omnis sequi cumque
+                nam.
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-              dicta alias id, nostrum rerum cum ducimus omnis sequi cumque nam.
-            </p>
-
-            <div className="flex w-full justify-between items-center">
-              <label>$ 50.99</label>
-              <Button variant="1">order now</Button>
+              <div className="flex w-full justify-between items-center">
+                <strong>$ 50.99</strong>
+                <Button variant="1">order now</Button>
+              </div>
             </div>
           </div>
         </div>
